b5b68dc0cec4a8929959cfaaed9a708e3034ccb4
elsaneth <elisabeth.suits@gmail.com>
2024-03-08 11:55:06 +0200

    change

--- ex3/1.3inch-OLED-HAT-Code/RaspberryPi/python/controller.py
+++ ex3/1.3inch-OLED-HAT-Code/RaspberryPi/python/controller.py
@@ -30,13 +30,10 @@ except IOError as e:
 broker="10.8.0.1"
 
 # Define callback
+# Define callback
 def on_message(client, userdata, message):
-    global disp, draw, font10, image1
-    draw.text((0, 0), str(message.payload.decode("utf-8")), font = font10, fill = 0)
-    disp.ShowImage(disp.getbuffer(image1))
-    print("received message =", str(message.payload.decode("utf-8")))
-    time.sleep(5)
-    disp.clear()
+    time.sleep(1)
+    print("received message =",str(message.payload.decode("utf-8")))
 
 client = paho.Client("client-001") 
 
@@ -53,56 +50,14 @@ print("subscribing ")
 client.subscribe("class/iot01") #subscribe
 time.sleep(2)
 
-# loop until exit with CTRL + C
 try:
         while True:
-                    time.sleep(1)
+                    time.sleep(5)
+                    for num in range(1, 13):
+                        padded_num = str(num).zfill(2)
+                        client.publish("class/iot" + padded_num, "Elisabeth tervitab")
 
 except KeyboardInterrupt:
     print("exiting")
-    client.disconnect() # disconnect
-    client.loop_stop()  # stop loop
-
-try:
-    disp = SH1106.SH1106()
-
-    print("\r\1.3inch OLED")
-    # Initialize library.
-    disp.Init()
-    # Clear display.
-    disp.clear()
-
-    # Create blank image for drawing.
-    image1 = Image.new('1', (disp.width, disp.height), "WHITE")
-    draw = ImageDraw.Draw(image1)
-    font = ImageFont.truetype('Font.ttf', 20)
-    font10 = ImageFont.truetype('Font.ttf',13)
-    print ("***draw line")
-    draw.line([(0,0),(127,0)], fill = 0)
-    draw.line([(0,0),(0,63)], fill = 0)
-    draw.line([(0,63),(127,63)], fill = 0)
-    draw.line([(127,0),(127,63)], fill = 0)
-    print ("***draw rectangle")
-    
-    print ("***draw text")
-    draw.text((30,0), 'Waveshare ', font = font10, fill = 0)
-    draw.text((28,20), u'微雪电子 ', font = font, fill = 0)
-
-    # image1=image1.rotate(180) 
-    disp.ShowImage(disp.getbuffer(image1))
-    time.sleep(2)
-    
-    print ("***draw image")
-    Himage2 = Image.new('1', (disp.width, disp.height), 255)  # 255: clear the frame
-    bmp = Image.open('pic.bmp')
-    Himage2.paste(bmp, (0,5))
-    # Himage2=Himage2.rotate(180) 	
-    disp.ShowImage(disp.getbuffer(Himage2))
-
-except IOError as e:
-    print(e)
-    
-except KeyboardInterrupt:    
-    print("ctrl + c:")
-    disp.RPI.module_exit()
-    exit()
+    client.disconnect() #disconnect
+    client.loop_stop() #stop loop
